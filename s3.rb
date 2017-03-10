99428ae83376c2a7017892251bf4a180db000373
vasilis <vasileiosvlachos@gmail.com>
2017-03-10 08:46:02 +0000

    It would appear that unlike Perl, methods need to be defined before they are called. So, help() moved towards the top.

--- s3.rb
+++ s3.rb
@@ -1,6 +1,46 @@
 require './lib/AwsS3'
 require 'getoptlong'
 
+def help
+    puts <<-EOF
+Usage: ruby s3.rb [OPTION]=[VALUE] ...
+
+OPTIONS
+
+    --action, -a
+            One of [upload, download, create].
+            upload  : upload a resource on S3. To be used alongside --aws_profile, --aws_region, --s3-bucket, --s3-key, --local-path.
+            download: download a resource from S3. To be used alongside --aws_profile, --aws_region, --s3-bucket, --s3-key, --local-path.
+            create  : create an S3 bucket. To be used alongside --aws-profile, --aws-region, --s3-bucket
+
+    --aws-profile, -i
+            This is the name of the IAM role listed in the shared credentials file (~/.aws/credentials).
+
+    --aws-region, -r
+            The AWS region where the S3 resource will be uploaded (--action=upload), downloaded (--action=download) from, or the region
+            where the S3 bucket is going to be created (--action=create).
+
+    --s3-bucket, -b
+            The name of the S3 bucket that will be used (--action=upload or --action=download), or be created (--action=create).
+
+    --s3-key, -k
+            The name of the resource to be uploaded (--action=upload) or downloaded (--action=download) form the S3 bucket specified (--s3-bucket).
+
+    --local-path, -p
+            The local path to the resource you wish to upload (--action=upload) to an S3 bucket, or the local path where you wish to download (--action=download)
+            from an S3 bucket. The S3 bucket is specified with --s3-bucket in both cases.
+
+    --help, -h
+            Print this help message
+
+AUTHOR / CONTRIBUTORS
+    synbit
+
+SOURCE
+    https://github.com/synbit/aws
+    EOF
+end
+
 opts = GetoptLong.new(
     ['--action', '-a', GetoptLong::REQUIRED_ARGUMENT],
     ['--aws-profile', '-i', GetoptLong::REQUIRED_ARGUMENT],
@@ -15,7 +55,7 @@ action, aws_profile, aws_region, s3_bucket, s3_key, local_path = nil
 
 opts.each do |opt, arg|
     case opt
-    when '--help'
+    when '--help', '-h'
         help()
     when '--action', '-a'
         action = arg
@@ -30,6 +70,11 @@ opts.each do |opt, arg|
     when '--local-path', '-p'
         local_path = arg
     end
+end
+
+if (action.nil? && aws_profile.nil? && aws_region.nil? && s3_bucket.nil? && s3_key.nil? && local_path.nil?)
+    puts("No argument provided...")
+    help()
 end
 
 s3 = AwsS3.new(
@@ -47,44 +92,4 @@ begin
 rescue StandardError.new("Something went wrong...") => e
     puts("#{e.class}\n#{e.message}")
     raise
-end
-
-def help
-    puts <<-EOF
-    Usage: ruby s3.rb [OPTION]=[VALUE] ...
-
-    OPTIONS
-
-        --action, -a
-                One of [upload, download, create].
-                upload  : upload a resource on S3. To be used alongside --aws_profile, --aws_region, --s3-bucket, --s3-key, --local-path.
-                download: download a resource from S3. To be used alongside --aws_profile, --aws_region, --s3-bucket, --s3-key, --local-path.
-                create  : create an S3 bucket. To be used alongside --aws-profile, --aws-region, --s3-bucket
-
-        --aws-profile, -i
-                This is the name of the IAM role listed in the shared credentials file (~/.aws/credentials).
-
-        --aws-region, -r
-                The AWS region where the S3 resource will be uploaded (--action=upload), downloaded (--action=download) from, or the region
-                where the S3 bucket is going to be created (--action=create).
-
-        --s3-bucket, -b
-                The name of the S3 bucket that will be used (--action=upload or --action=download), or be created (--action=create).
-
-        --s3-key, -k
-                The name of the resource to be uploaded (--action=upload) or downloaded (--action=download) form the S3 bucket specified (--s3-bucket).
-
-        --local-path, -p
-                The local path to the resource you wish to upload (--action=upload) to an S3 bucket, or the local path where you wish to download (--action=download)
-                from an S3 bucket. The S3 bucket is specified with --s3-bucket in both cases.
-
-        --help, -h
-                Print this help message
-
-    AUTHOR / CONTRIBUTORS
-        synbit
-
-    SOURCE
-        https://github.com/synbit/aws
-    EOF
 end
