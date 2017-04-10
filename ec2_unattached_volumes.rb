017631692f058d9786123283f77c6af502e3a088
vasilis <vasileiosvlachos@gmail.com>
2017-04-11 00:16:30 +0100

    Added help menu to ec2_unattached_volumes.rb

--- ec2_unattached_volumes.rb
+++ ec2_unattached_volumes.rb
@@ -1,4 +1,33 @@
 require 'aws-sdk'
+require 'getoptlong'
+
+def help
+    puts <<-EOF
+NAME
+    ec2_unattached_volumes - Find unattached EC2 volumes on AWS
+
+SYNOPSIS
+    ruby ec2_unattached_volumes [options]
+
+DESCRIPTION
+    ec2_unattached_volumes searches all EC2 volumes with "status": "available" against an AWS and reports the findings.
+    It requires AWS Shared Credentials to be setup on the local machine ($HOME/.aws/credentials).
+
+OPTIONS
+    --aws-profile, -i
+            This is the name of the IAM role listed in the shared credentials file (~/.aws/credentials).
+
+    --aws-region, -r
+            The AWS region where you want to search for "available" EC2 volumes.
+
+AUTHORS / CONTRIBUTORS
+    synbit
+
+SOURCE
+    https://github.com/synbit/aws
+EOF
+end
+
 
 iam = 'xxx'
 region = 'yyy'
