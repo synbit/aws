69404d030c8480fc4ead00052167804d40abd42b
vasilis <vasileiosvlachos@gmail.com>
2017-04-11 01:10:28 +0100

    Handing options/arguments and fixed layout.

--- ec2_unattached_volumes.rb
+++ ec2_unattached_volumes.rb
@@ -31,10 +31,46 @@ SOURCE
 EOF
 end
 
+opts = GetoptLong.new(
+    ['--aws-profile', '-i', GetoptLong::REQUIRED_ARGUMENT],
+    ['--aws-region', '-r', GetoptLong::REQUIRED_ARGUMENT],
+    [ '--help', '-h', GetoptLong::NO_ARGUMENT ]
+)
 
-iam = 'xxx'
-region = 'yyy'
-vols = []
-profile = Aws::SharedCredentials.new(profile_name: iam)
-ec2 = Aws::EC2::Client.new( region: region, credentials: profile.credentials )
-ec2.describe_volumes({ filters: [{name: "status", values: ["available"]}] }).volumes.map { |v| vols << v.volume_id }
+aws_profle, aws_region = nil
+res = []
+
+opts.each do |opt, arg|
+    case opt
+    when '--help', '-h'
+        help()
+    when '--aws-profile', '-i'
+        aws_profile = arg
+    when '--aws-region', '-r'
+        aws_region = arg
+    end
+end
+
+if (aws_profle.nil? or aws_region.nil?)
+    abort(help())
+end
+
+profile = Aws::SharedCredentials.new(
+    profile_name: aws_profile
+)
+
+ec2 = Aws::EC2::Client.new(
+    region: aws_region,
+    credentials: profile.credentials
+)
+
+ec2.describe_volumes({
+    filters: [
+        {
+            name: "status",
+            values: ["available"]
+        }
+    ]
+}).volumes.map { |v| res << v.volume_id }
+
+puts(res)
