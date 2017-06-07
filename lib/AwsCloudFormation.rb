a42e20ec6ba73bab8bb2b983dcb06cd4791a88f9
vasilis <vasileiosvlachos@gmail.com>
2017-06-07 16:44:33 +0100

    Added the necessary private methods in order to load the right profile and interact with AWS CF API.

--- lib/AwsCloudFormation.rb
+++ lib/AwsCloudFormation.rb
@@ -8,4 +8,20 @@ class AwsCloudFormation
         @aws_profile = aws_profile
         @aws_region = aws_region
     end
+
+    private
+    def load_profile
+        profile = Aws::SharedCredentials.new(
+            profile_name: @aws_profile,
+            region: @aws_region
+        )
+    end
+
+    def cf_api
+        profile = load_profile
+        cf = Aws::CloudFormation::Client.new(
+            region: @aws_region,
+            credentials: profile.credentials
+        )
+    end
 end
